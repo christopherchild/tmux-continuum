7efcc84a46fe5fcfc0bc2887851f2092c3307940
alexg9010 <alex.gos90@gmail.com>
2020-02-07 15:13:46 +0100

    adjust status message
    - change continuum status message to show minutes since last save

--- scripts/continuum_status.sh
+++ scripts/continuum_status.sh
@@ -7,11 +7,14 @@ source "$CURRENT_DIR/variables.sh"
 
 print_status() {
 	local save_int="$(get_tmux_option "$auto_save_interval_option")"
+	local last_save_stamp="$(get_tmux_option $last_auto_save_option )"
+	local current_stamp="$( date +%s )"
+	local duration="$( bc <<< "$current_stamp - $last_save_stamp")"
 	local status=""
 	local style_wrap
-	if [ $save_int -gt 0 ]; then
+	if [ $duration -gt 0 ]; then
 		style_wrap="$(get_tmux_option "$status_on_style_wrap_option" "")"
-		status="$save_int"
+		status="saved $(date +%M -d @"$duration" ) minutes ago"
 	else
 		style_wrap="$(get_tmux_option "$status_off_style_wrap_option" "")"
 		status="off"
