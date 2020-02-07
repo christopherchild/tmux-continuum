111db9c611c976be0483fd2ec2732620ba7d1c8c
alexg9010 <alex.gos90@gmail.com>
2020-02-07 15:12:40 +0100

    fix multi-user issue
    - check only for user-specific tmux processes
    - ignore tmux a|attach processes

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -29,8 +29,19 @@ all_tmux_processes() {
 		\grep -v "^tmux source"
 }
 
+user_tmux_processes() {
+	# selects only user processes
+	# ignores `tmux source-file .tmux.conf` command used to reload tmux.conf
+	# ignores `tmux a` or `tmux attach` to connect to running session
+	ps -u $UID -o "command pid" | 
+		\grep "^tmux" | 
+		\grep -v "^tmux source" | 
+		\grep -v -e "^tmux a" -e "^tmux attach"
+
+}
+
 number_tmux_processes_except_current_server() {
-	all_tmux_processes |
+	user_tmux_processes |
 		\grep -v " $(current_tmux_server_pid)$" |
 		wc -l |
 		sed "s/ //g"
