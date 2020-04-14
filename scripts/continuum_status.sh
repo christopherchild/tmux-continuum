dc60f06404bfc28afdf990a069c4dab2a6f34133
Alexander Blume <alex.gos90@gmail.com>
2020-04-14 21:58:30 +0200

    format date string dependend on os

--- scripts/continuum_status.sh
+++ scripts/continuum_status.sh
@@ -5,6 +5,15 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 source "$CURRENT_DIR/variables.sh"
 
+format_time() {
+	case "$OSTYPE" in
+		darwin*)
+			echo $(date -r "$1" +%M);;
+		*)
+			echo $(date +%M -d @"$1");;	
+	esac
+}
+
 print_status() {
 	local last_save_stamp="$(get_tmux_option $last_auto_save_option )"
 	local current_stamp="$( date +%s )"
@@ -13,7 +22,7 @@ print_status() {
 	local style_wrap
 	if [ $duration -gt 0 ]; then
 		style_wrap="$(get_tmux_option "$status_on_style_wrap_option" "")"
-		status="saved $(date +%M -d @"$duration" ) minutes ago"
+		status="saved $(format_time "$duration" ) minutes ago"
 	else
 		style_wrap="$(get_tmux_option "$status_off_style_wrap_option" "")"
 		status="off"
@@ -24,4 +33,6 @@ print_status() {
 	fi
 	echo "$status"
 }
+
+
 print_status
